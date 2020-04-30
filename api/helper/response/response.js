6a1244fcf11e0b990f8f28c60c765faf8d04328b
Ujjwal Gupta <guptaujjwal515@gmail.com>
2020-04-30 17:13:48 +0530

    Add files via upload

--- api/helper/response/response.js
+++ api/helper/response/response.js
@@ -1,1 +1,12 @@
-
+exports.apiResponse = (res, status, message, code, data, token) => {
+    const resModel = {
+        meta: {
+            Status: status ? status : false,
+            Message: message ? message : "",
+            Code: code ? code : 200
+        },
+        Data: data ? data : {},
+        Token: token ? token : ""
+    };
+    res.json(resModel);
+};
